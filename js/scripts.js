8bd14c49512de64c715469762c29bddbeb3e22f0
hwany-j <jhbang1422@gmail.com>
2022-02-15 13:54:40 +0900

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Resume v6.0.3 (https://hwany-j.github.io)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
 (function ($) {
